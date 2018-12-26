de4d10d107e24e21ff57d0c3d1b1b405e1d1f790
Sergii Prudkiy <s.prudkiy@yandex.ua>
2018-12-26 22:27:13 +0100

    gulp instal finish pacet

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat       = require('gulp-concat'),
+    sass         = require('gulp-sass'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    gulpIf       = require('gulp-if'),
+    browserSync  = require('browser-sync').create();
+
+var config = {
+    path: {
+        scss: './src/scss/**/*.scss',
+        html: './public/index.html'
+    },
+    output: {
+        cssName: 'bundle.min.css',
+        path: './public',
+        pathCss: './public/styles'
+    }
+};
+
+gulp.task('scss', function() {
+    return gulp.src(config.path.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat(config.output.cssName))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.output.pathCss))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+
+    gulp.watch(config.path.scss, gulp.parallel('scss'));
+    gulp.watch(config.path.html).on('change', browserSync.reload);
+
+});
+
+gulp.task('default', gulp.parallel('scss', 'serve'));
