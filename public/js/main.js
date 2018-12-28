dd05fa33e16086344f89fc5b404c4c43a9ede90c
Sergii Prudkiy <s.prudkiy@yandex.ua>
2018-12-29 00:31:24 +0100

    work ...

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('work main.js');
+// navitgation button menu mobile
+$( document ).ready(function() {
+    let nav__menu_mobStatus = false;
+    let ulNavHeader = $('nav ul');
+    $('#nav__menu_mob').click(function(){
+        if(!nav__menu_mobStatus) {
+           ulNavHeader.css('display', 'block');
+           nav__menu_mobStatus = true;
+        }
+        else {
+          ulNavHeader.css('display', 'none');
+          nav__menu_mobStatus = false;
+        }
+    });
+
+});
+
